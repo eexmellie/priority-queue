071346a4d140b13773820e32f618d51c39210896
Volha Kazlovich <eexmellie@gmail.com>
2019-03-03 00:30:27 +0300

    feat: add Node class implementation

--- src/node.js
+++ src/node.js
@@ -1,23 +1,77 @@
 class Node {
-	constructor(data, priority) {
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	}
+  appendChild(node) {
+    if (this.left) {
+      if (this.right) {
+        return false;
+      }
+      this.right = node;
+      node.parent = this;
+    } else {
+      this.left = node;
+      node.parent = this;
+    }
+  }
 
-	appendChild(node) {
+  removeChild(node) {
+    if (this.left === node) {
+      this.left = null;
+      node.parent = null;
+    } else if (this.right === node) {
+      this.right = null;
+      node.parent = null;
+    } else {
+      throw "No such child exists"
+    }
+  }
 
-	}
+  remove() {
+    if (this.parent) {
+      this.parent.removeChild(this);
+    }
+  }
 
-	removeChild(node) {
+  swapWithParent() {
+    if (this.parent) {
+      const leftChild = this.left;
+      const rightChild = this.right;
+      const parent = this.parent;
+      const grandParent = parent.parent;
+      const parentLeftChild = parent.left;
+      const parentRightChild = parent.right;
 
-	}
+      this.parent = grandParent;
+      
+      if (parent.left === this) {
+        this.left = parent;
+        this.right = parentRightChild;
+        if (parentRightChild) parentRightChild.parent = this;
+      } else {
+        this.right = parent;
+        this.left = parentLeftChild;
+        if (parentLeftChild) parentLeftChild.parent = this;
+      }
 
-	remove() {
+      if (grandParent && grandParent.left === parent) {
+        grandParent.left = this;
+      } else if (grandParent && grandParent.right === parent) {
+        grandParent.right = this;
+      }
 
-	}
-
-	swapWithParent() {
-		
-	}
+      parent.parent = this;
+      parent.left = leftChild;
+      parent.right = rightChild;
+      if (leftChild) leftChild.parent = parent;
+      if (rightChild) rightChild.parent = parent;
+    }
+  }
 }
 
 module.exports = Node;
