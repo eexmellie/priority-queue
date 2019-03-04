c8628f3560813db3594cb07624db7e239c3ffd77
Volha Kazlovich <eexmellie@gmail.com>
2019-03-05 01:26:22 +0300

    feat: implement queue class

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,31 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize ? maxSize : 30;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if (this.heap.size() < this.maxSize) {
+			this.heap.push(data, priority);
+		} else {
+			throw 'Queue max size reached!';
+		}
 	}
 
 	shift() {
-
+		if (this.isEmpty()) {
+			throw 'Queue is empty!'
+		}
+		return this.heap.pop();
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
